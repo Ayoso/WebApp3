b9ff3c46ced339a59cd3df0d92d6a5c665fc614d
Aziz <azekakaratay04@gmail.com>
2024-06-23 01:04:48 +0600

    h

--- server.js
+++ server.js
@@ -4,47 +4,38 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3002; // Измените порт на 3002 или любой другой доступный порт
+const PORT = process.env.PORT || 3002;
 
 app.use(bodyParser.json());
-app.use(cors()); // Добавьте эту строку для разрешения CORS
-
-// Обслуживание статических файлов из папки 'public'
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public'))); // Добавьте это для обслуживания статических файлов из папки 'public'
 
 let currentCoefficients = generateRandomCoefficients(); // Инициализация случайными коэффициентами
 
 // Эндпоинт для получения коэффициентов
 app.post('/get-coefficients', (req, res) => {
     try {
-        // Обновить текущие коэффициенты случайными значениями
         currentCoefficients = generateRandomCoefficients();
         console.log('Отправка коэффициентов:', currentCoefficients);
-
-        // Отправить коэффициенты в формате JSON
-        res.json({ coefficient1: parseFloat(currentCoefficients.coefficient1), coefficient2: parseFloat(currentCoefficients.coefficient2) });
+        res.json({
+            coefficient1: parseFloat(currentCoefficients.coefficient1),
+            coefficient2: parseFloat(currentCoefficients.coefficient2)
+        });
     } catch (error) {
         console.error('Ошибка при генерации коэффициентов:', error);
         res.status(500).json({ error: 'Ошибка при генерации коэффициентов' });
     }
 });
 
-// Обслуживание корневого маршрута
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-// Функция генерации случайных коэффициентов
 function generateRandomCoefficients() {
     let coefficient1, coefficient2;
     do {
-        coefficient1 = (Math.random() * 2.7 + 2.3).toFixed(2); // Генерация случайного числа от 2.3 до 5.0
-        coefficient2 = (Math.random() * 3.5 + parseFloat(coefficient1) + 1).toFixed(2); // Генерация случайного числа от coefficient1 + 1 до coefficient1 + 4.5
-    } while (parseFloat(coefficient1) >= parseFloat(coefficient2)); // Убедиться, что coefficient2 больше coefficient1
+        coefficient1 = (Math.random() * 2.7 + 2.3).toFixed(2);
+        coefficient2 = (Math.random() * 3.5 + parseFloat(coefficient1) + 1).toFixed(2);
+    } while (parseFloat(coefficient1) >= parseFloat(coefficient2));
     return { coefficient1, coefficient2 };
 }
 
-// Запуск сервера
 app.listen(PORT, () => {
     console.log(`Сервер работает на порту ${PORT}`);
 }).on('error', (err) => {
