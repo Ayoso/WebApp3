fda2f31ed685a52f57cdb6498f0629f2f9914eb1
Aziz <azekakaratay04@gmail.com>
2024-06-22 15:53:09 +0600

    innit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const webAppUrl = 'https://legendary-bombolone-18e5fd.netlify.app'; // Использование локального прокси
+const webAppUrl = 'https://cors-anywhere.herokuapp.com/https://legendary-bombolone-18e5fd.netlify.app'; // Использование публичного прокси
 
 const timeContainer = document.getElementById('timeContainer');
 const chanceContainer = document.getElementById('chanceContainer');
