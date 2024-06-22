5b400b294734047b8e4d23e0ab6b209fb7b5d9e7
Aziz <azekakaratay04@gmail.com>
2024-06-22 10:40:39 +0600

    Innit

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const webAppUrl = 'http://localhost:3002'; // Измените на публичный URL-адрес вашего сервера, если он развернут
+const webAppUrl = 'https://legendary-bombolone-18e5fd.netlify.app'; // Замените на публичный URL-адрес вашего сервера
 
-const coefficientsContainer = document.getElementById('coefficientsContainer');
+document.getElementById('coefficientsContainer');
 const timeContainer = document.getElementById('timeContainer');
 const chanceContainer = document.getElementById('chanceContainer');
 const loaderBar = document.querySelector('.loader-bar');
 const getSignalButton = document.getElementById('getSignalButton');
-const airplane = document.querySelector('.airplane'); // Добавляем самолёт
+const airplane = document.querySelector('.airplane');
 
-let loadingFinished = true; // Initial value set to true
+let loadingFinished = true;
 
 function updateData(coefficients) {
     if (coefficients) {
@@ -15,8 +15,8 @@ function updateData(coefficients) {
         let coefficient2 = parseFloat(coefficients.coefficient2).toFixed(2);
 
         while (parseFloat(coefficient1) >= parseFloat(coefficient2) || parseFloat(coefficient2) - parseFloat(coefficient1) < 2) {
-            coefficient1 = (Math.random() * 3 + 2).toFixed(2); // Generate random number between 2 and 5
-            coefficient2 = (Math.random() * 3 + 5).toFixed(2); // Generate random number between 5 и 8
+            coefficient1 = (Math.random() * 3 + 2).toFixed(2);
+            coefficient2 = (Math.random() * 3 + 5).toFixed(2);
         }
 
         document.getElementById('coefficient1').textContent = `${coefficient1}X`;
@@ -42,11 +42,11 @@ function fetchCoefficients() {
         .then(data => {
             updateData(data);
             loadingFinished = true;
-            loaderBar.style.animation = 'none'; // Stop animation after receiving data
-            loaderBar.style.width = '0'; // Reset the loader width
-            airplane.style.animation = 'none'; // Stop airplane animation after receiving data
-            void airplane.offsetWidth; // Trigger reflow
-            airplane.style.animation = 'airplaneAnimation 10s linear'; // Restart airplane animation
+            loaderBar.style.animation = 'none';
+            loaderBar.style.width = '0';
+            airplane.style.animation = 'none';
+            void airplane.offsetWidth;
+            airplane.style.animation = 'airplaneAnimation 10s linear';
         })
         .catch(error => {
             console.error('Error fetching coefficients:', error);
@@ -56,23 +56,21 @@ function fetchCoefficients() {
 getSignalButton.addEventListener('click', () => {
     if (loadingFinished) {
         loadingFinished = false;
-        loaderBar.style.animation = 'none'; // Reset animation
-        void loaderBar.offsetWidth; // Trigger reflow
-        loaderBar.style.animation = 'loadAnimation 10s linear'; // Start loading animation
-        airplane.style.animation = 'none'; // Reset airplane animation
-        void airplane.offsetWidth; // Trigger reflow
-        airplane.style.animation = 'airplaneAnimation 10s linear'; // Start airplane animation
+        loaderBar.style.animation = 'none';
+        void loaderBar.offsetWidth;
+        loaderBar.style.animation = 'loadAnimation 10s linear';
+        airplane.style.animation = 'none';
+        void airplane.offsetWidth;
+        airplane.style.animation = 'airplaneAnimation 10s linear';
         setTimeout(() => {
-            fetchCoefficients(); // Fetch new coefficients after 10 seconds
-        }, 10000); // Ускорение анимации загрузки
+            fetchCoefficients();
+        }, 10000);
     }
 });
 
-// Function to update time in real-time
 function updateTime() {
     const currentTime = new Date();
     timeContainer.textContent = `Time: ${currentTime.toLocaleTimeString()}`;
 }
 
-// Update time every second
 setInterval(updateTime, 1000);
